086805d16beca8a735d98c9338fdd38f1854ff80
cosra-bit <alejandromonak@gmail.com>
2023-05-21 12:59:57 -0500

    Optimización y buen funcionamiento del encriptador

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
-var encriptado = CryptoJS.AES.encrypt("mensaje");
-var desencriptado = CryptoJS.AES.decrypt(encriptado, "mensaje secreto");
+function encriptar (){
+    var texto = document.querySelector("#entradatexto").value;
+    var textoencriptado = texto.replace(/a/gi, "ai").replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/o/gi, "ober").replace(/u/gi, "ufat");
+    document.querySelector(".salidatexto").value = textoencriptado;
+    document.querySelector("#entradatexto").value;
+    }
 
-/* 
-var cambiante = document.getElementById("Bienvenidos a mi Encriptador");
-var keyframes = [
-    { opacity: 1},
-    { opacity: 0},
-];
-var opciones = {
-    duration: 200,
-    iterations: Infinity,
-    direction: "alternate",
-};
-cambiante.animate(keyframes, opciones);
-*/
+
+
+var botonencriptar = document.querySelector(".botonencriptar"); botonencriptar.onclick = encriptar;
+function desencriptar (){ 
+    var texto = document.querySelector("#entradatexto").value; 
+    var textoencriptado = texto.replace(/ai/gi, "a").replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ober/gi, "o").replace(/ufat/gi, "u"); 
+    document.querySelector(".salidatexto").value = textoencriptado; 
+    document.querySelector("#entradatexto").value;
+    
+}
+    
+var botondesencriptar = document.querySelector(".botondesencriptar"); botondesencriptar.onclick = desencriptar;
